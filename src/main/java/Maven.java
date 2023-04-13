67422db63806c459710dc5a9fc5802110817c58b
Daniela Salas <danielapiconsalas@gmail.com>
2023-04-13 14:10:56 -0500

    checks if user input is a number

--- src/main/java/Maven.java
+++ src/main/java/Maven.java
@@ -1,10 +1,21 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class Maven {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        //asks the user for input and outputs the user input
         System.out.println("Enter something: ");
         String userInput = input.nextLine();
+        System.out.println("You entered: " + userInput);
+
+        //checks if its a number
+        boolean numOrNot = StringUtils.isNumeric(userInput);
+        String answerToNum = numOrNot ? "is a number" : "is not a number";
+        System.out.print(userInput + " " + answerToNum);
+
+
     }
 }
